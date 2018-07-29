8f80834f2cd8f6004ae036fa6c1d442022036dec
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-07-29 22:21:13 +0200

    Fixed restart supervisor command DI factory
    It was never working to beging with 🤐

--- config/di/supervisor.php
+++ config/di/supervisor.php
@@ -1,13 +1,29 @@
 <?php
 
-use ApiClients\Client\Supervisord\AsyncClientInterface;
+use ApiClients\Client\Supervisord\AsyncClient;
 use Psr\Log\LoggerInterface;
+use React\EventLoop\LoopInterface;
 use ReactiveApps\Command\Supervisor\RestartSupervisor;
 use ReactiveApps\Rx\Shutdown;
 
 return [
-    RestartSupervisor::class => \DI\factory(function (AsyncClientInterface $supervisor, LoggerInterface $logger, Shutdown $shutdown, string $name) {
-        return new RestartSupervisor($supervisor, $logger, $shutdown, $name);
+    RestartSupervisor::class => \DI\factory(function (
+        LoopInterface $loop,
+        LoggerInterface $logger,
+        Shutdown $shutdown,
+        string $host,
+        string $name
+    ) {
+        return new RestartSupervisor(
+            AsyncClient::create(
+                $host,
+                $loop
+            ),
+            $logger,
+            $shutdown,
+            $name
+        );
     })
-    ->parameter('name', \DI\get('config.supervisor.process.name')),
+        ->parameter('name', \DI\get('config.supervisor.process.name'))
+        ->parameter('host', \DI\get('config.supervisor.process.host')),
 ];
