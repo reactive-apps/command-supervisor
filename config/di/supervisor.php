37adc1f58cfee7082a39f6fdaf1c7a562fe86cfa
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-03-09 12:39:29 +0100

    Use new php-di-definitions/api-clients-supervisord for supervisord client

--- config/di/supervisor.php
+++ config/di/supervisor.php
@@ -1,29 +1,23 @@
 <?php
 
-use ApiClients\Client\Supervisord\AsyncClient;
+use ApiClients\Client\Supervisord\AsyncClientInterface;
 use Psr\Log\LoggerInterface;
-use React\EventLoop\LoopInterface;
 use ReactiveApps\Command\Supervisor\RestartSupervisor;
 use ReactiveApps\Rx\Shutdown;
 
 return [
     RestartSupervisor::class => \DI\factory(function (
-        LoopInterface $loop,
         LoggerInterface $logger,
         Shutdown $shutdown,
-        string $host,
+        AsyncClientInterface $supervisord,
         string $name = ''
     ) {
         return new RestartSupervisor(
-            AsyncClient::create(
-                $host,
-                $loop
-            ),
+            $supervisord,
             $logger,
             $shutdown,
             $name
         );
     })
-        ->parameter('name', \DI\get('config.supervisor.process.name'))
-        ->parameter('host', \DI\get('config.supervisor.process.host')),
+        ->parameter('name', \DI\get('config.supervisor.process.name')),
 ];
